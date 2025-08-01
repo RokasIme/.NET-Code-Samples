82c6728cd366524bb28a666d0404fcc61d09820f
Rokas Imelinskas <Rokas.Imelinskas@gmail.com>
2025-08-01 13:05:40 +0300

    Homework book store add, list, delete

---
+++ Console/BookStoreApplication/BookStoreApplication/Modules/BookStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApplication.Modules
+{
+    public class Book
+    {
+        public string Name { get; set; }
+                
+        public string Description { get; set; }
+
+        public int Amount { get; set; }
+
+        public Book(string name, string description, int amount)
+        { 
+            Name = name;
+            Description = description;
+            Amount = amount;
+        }
+        
+    }
+}
--- Console/BookStoreApplication/BookStoreApplication/Program.cs
+++ Console/BookStoreApplication/BookStoreApplication/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using BookStoreApplication.Modules;
+
+var bookList = new List<Book>();
+
+while (true)
+{
+   
+
+    Console.WriteLine("Please enter 'Add' or 'List' to start application or 'Del' for delete book from store");
+    var command = Console.ReadLine()?.ToLower();
+
+    if (command == "add")
+    {
+        Console.WriteLine("Please enter the book Name");
+        var name = Console.ReadLine();
+        var sameName = bookList.Where(b => b.Name == name)
+            .ToList();
+        if ( sameName.Count > 0) 
+                {
+            Console.WriteLine("Book with this name already exists.");
+            continue;
+        }
+
+        Console.WriteLine("Please enter the Description");
+        var description = Console.ReadLine();
+
+        Console.WriteLine("Please enter the Amount of books");
+        var amount = Console.ReadLine();
+
+        var book = new Book(name, description, int.Parse(amount));
+        bookList.Add(book);
+    }
+    else if (command == "list")
+    {
+        foreach (var book in bookList)
+        {
+            Console.WriteLine($"Book name: {book.Name}, Description: {book.Description}, amount: {book.Amount}");
+        }
+    }
+    else if (command == "del")
+    {
+        Console.WriteLine("Please enter the book Name to delete");
+        var nameToDelete = Console.ReadLine();
+        var bookToDelete = bookList.FirstOrDefault(b => b.Name == nameToDelete);
+        
+        if (bookToDelete != null)
+        {
+            bookList.Remove(bookToDelete);
+            Console.WriteLine($"Book '{nameToDelete}' has been deleted.");
+        }
+        else
+        {
+            Console.WriteLine($"Book '{nameToDelete}' not found.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid command. Please enter 'Add' or 'List'.");
+    }
+
+  
+
+
+}
